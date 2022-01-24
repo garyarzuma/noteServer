b393acc267b83e8fa897d4bb935260bd8f8bba8d
garyarzumanyan <garyarzumanyan@gmail.com>
2022-01-23 23:04:52 -0800

    Updated backed to use mongo for all services

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(cors())
 
@@ -36,58 +38,45 @@ let notes = [
   }
   app.use(requestLogger)
  
-
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-  
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
+  })
+
+  app.get('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
   
   app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
+    Note.findByIdAndRemove(request.params.id)
+    .then (note => response.status(204).end())
   })
-  
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
   
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
+    if (body.content === undefined) {
       return response.status(400).json({ 
         error: 'content missing' 
       })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
       date: new Date(),
-      id: generateId(),
-    }
+    })
   
-    notes = notes.concat(note)
-    console.log(note)
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
   const unknownEndpoint = (request, response) => {
@@ -96,7 +85,7 @@ let notes = [
   
   app.use(unknownEndpoint)
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
