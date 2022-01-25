27d507d0e316b3f011890c5011a66031165408b9
garyarzumanyan <garyarzumanyan@gmail.com>
2022-01-24 21:59:47 -0800

    Added error handling functionality

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const Note = require('./models/note')
 
 app.use(cors())
 
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 
-  const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
-  app.use(requestLogger)
- 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger) 
+
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
@@ -28,15 +28,39 @@ app.use(express.static('build'))
     })
   })
 
-  app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
-    })
+  app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+      .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
   })
   
-  app.delete('/api/notes/:id', (request, response) => {
+  app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndRemove(request.params.id)
-    .then (note => response.status(204).end())
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
   })
   
   app.post('/api/notes', (request, response) => {
@@ -64,6 +88,18 @@ app.use(express.static('build'))
   }
   
   app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  app.use(errorHandler)
 
   const PORT = process.env.PORT
 app.listen(PORT, () => {
